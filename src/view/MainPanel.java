8831979c1949f32af049d58c2ac4b176433ba070
Fleny <fleny113@outlook.com>
2026-03-25 22:42:28 +0100

    Aggiungi overlay per messaggi

--- src/view/MainPanel.java
+++ src/view/MainPanel.java
@@ -1,45 +1,113 @@
 package view;
 
 import javax.swing.JPanel;
+import javax.swing.JTextArea;
+import javax.swing.OverlayLayout;
+
 import java.awt.CardLayout;
 import java.awt.Color;
+import java.awt.BorderLayout;
+
+import javax.swing.JLayeredPane;
+import javax.swing.BoxLayout;
+import java.awt.Component;
+import javax.swing.Box;
+import javax.swing.border.EmptyBorder;
+import java.awt.Font;
 
 @SuppressWarnings("serial")
 public class MainPanel extends JPanel {
-    private CardLayout cardLayout;
-    private MenuPanel menuPanel;
-    private SalaPanel salaPanel;
-    private ClassificaPanel classificaPanel;
-    private CucinaPanel cucinaPanel;
+	private CardLayout cardLayoutMainUI;
+	private MenuPanel menuPanel;
+	private SalaPanel salaPanel;
+	private ClassificaPanel classificaPanel;
+	private CucinaPanel cucinaPanel;
+	private JLayeredPane layeredPane;
+	private JPanel overlayUI;
+	private JPanel mainUI;
 
-    public MainPanel() {
-    	cardLayout = new CardLayout(0, 0);
-        setLayout(cardLayout);
-        
-        menuPanel = new MenuPanel();
-        add(menuPanel, "menu");
+	public MainPanel() {
+		setLayout(new BorderLayout(0, 0));
 
-        salaPanel = new SalaPanel();
-        add(salaPanel, "sala");
+		// Il LayeredPane consente di avere 2 cose una sopra l'altra
+		layeredPane = new JLayeredPane();
+		// L'overlayLayout consente di posizionare 2 cose una sopra l'altra senza
+		// absolute (default di JLayeredPane)
+		layeredPane.setLayout(new OverlayLayout(layeredPane));
+		add(layeredPane, BorderLayout.CENTER);
 
-        classificaPanel = new ClassificaPanel();
-        add(classificaPanel, "classifica");
-        
-        cucinaPanel = new CucinaPanel();
-        add(cucinaPanel, "cucina");
-    }
-    
-    public void setStatoTavolo(int tavolo, int stato) {
-    	switch(stato) {
-    		case 1:
-    			salaPanel.getTavolo(tavolo).setBackground(Color.green);
-    			break;
-    		case 2:
-    			salaPanel.getTavolo(tavolo).setBackground(Color.yellow);
-    			break;
-    		case 3:
-    			salaPanel.getTavolo(tavolo).setBackground(Color.red);
-    			break;
-    	}
-    }
+		mainUI = new JPanel();
+		overlayUI = new JPanel();
+
+		// Aggiungi le 2 UI al layeredPane
+		layeredPane.add(mainUI, JLayeredPane.DEFAULT_LAYER);
+		layeredPane.add(overlayUI, JLayeredPane.PALETTE_LAYER);
+
+		// L'overlay layout funziona in maniera un po particolare, quindi per mettere
+		// l'overlay in alto a destra, tutto deve essere allinato in alto a destra
+		mainUI.setAlignmentY(Component.TOP_ALIGNMENT);
+		mainUI.setAlignmentX(Component.RIGHT_ALIGNMENT);
+		overlayUI.setAlignmentY(Component.TOP_ALIGNMENT);
+		overlayUI.setAlignmentX(Component.RIGHT_ALIGNMENT);
+
+		// --- OVERLAY UI ---
+		overlayUI.setOpaque(false); // Disabilita lo sfondo
+		overlayUI.setBorder(new EmptyBorder(10, 10, 10, 10));
+		overlayUI.setLayout(new BoxLayout(overlayUI, BoxLayout.Y_AXIS));
+
+		// Messaggii di test
+		overlayUI.add(creaMessaggioOverlay("Tutorial: Hello world!"));
+		overlayUI.add(Box.createVerticalStrut(5));
+		overlayUI.add(creaMessaggioOverlay("Tutorial:\nPer cucinare qualcosa in cucina fai click destro su un forno"));
+
+		// --- MAIN UI ---
+		cardLayoutMainUI = new CardLayout(0, 0);
+		mainUI.setLayout(cardLayoutMainUI);
+
+		menuPanel = new MenuPanel();
+		mainUI.add(menuPanel, "menu");
+
+		cucinaPanel = new CucinaPanel();
+		mainUI.add(cucinaPanel, "cucina");
+
+		salaPanel = new SalaPanel();
+		mainUI.add(salaPanel, "sala");
+
+		classificaPanel = new ClassificaPanel();
+		mainUI.add(classificaPanel, "classifica");
+	}
+
+	private JTextArea creaMessaggioOverlay(String testo) {
+		JTextArea textArea = new JTextArea(testo);
+
+		textArea.setBorder(new EmptyBorder(5, 5, 5, 5));
+		// Dato che non sappiamo che font sia in uso, usa quello di default ma con il
+		// bold
+		textArea.setFont(textArea.getFont().deriveFont(textArea.getFont().getStyle() | Font.BOLD));
+		textArea.setEnabled(false);
+		textArea.setEditable(false);
+		// Di default il testo disattivato ha un colore grigio, reimposta a nero
+		textArea.setDisabledTextColor(Color.BLACK);
+		textArea.setAlignmentX(Component.RIGHT_ALIGNMENT);
+		textArea.setBackground(Color.YELLOW);
+		// getPreferredSize è calcolato in base al testo, se impostiamo il max a
+		// preferred gli diamo lo spazio minimo
+		textArea.setMaximumSize(textArea.getPreferredSize());
+
+		return textArea;
+	}
+
+	public void setStatoTavolo(int tavolo, int stato) {
+		switch (stato) {
+		case 1:
+			salaPanel.getTavolo(tavolo).setBackground(Color.green);
+			break;
+		case 2:
+			salaPanel.getTavolo(tavolo).setBackground(Color.yellow);
+			break;
+		case 3:
+			salaPanel.getTavolo(tavolo).setBackground(Color.red);
+			break;
+		}
+	}
 }
