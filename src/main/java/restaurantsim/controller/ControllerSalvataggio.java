85beaccddd382e6f47e477c428645adfdaef026b
TheTinkerersHaven <93077613+TheTinkerersHaven@users.noreply.github.com>
2026-04-29 16:54:54 +0200

    Creato base per salvataggi, da implementare in futuro (l'oggetto da creare è nel model, e va creato dal controller che ci inserisce i dati)

---
+++ src/main/java/restaurantsim/controller/ControllerSalvataggio.java
@@ -0,0 +1,16 @@
+package restaurantsim.controller;
+
+import restaurantsim.model.Sala;
+import restaurantsim.view.MainPanel;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import java.io.File;
+
+public class ControllerSalvataggio {
+    // Questo raccoglie i dati da panel e model e li dà al model Salvataggio.java
+    private MainPanel mainPanel;
+    private Sala salaModel;
+
+
+}
