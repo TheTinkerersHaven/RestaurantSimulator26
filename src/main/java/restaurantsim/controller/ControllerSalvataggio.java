d3b793b3ae8f22ebf102d1d89ac4d667d8ca8160
TheTinkerersHaven <93077613+TheTinkerersHaven@users.noreply.github.com>
2026-05-01 14:47:16 +0200

    feat: aggiungi cuochi e classifica nel modello di salvataggio

--- src/main/java/restaurantsim/controller/ControllerSalvataggio.java
+++ src/main/java/restaurantsim/controller/ControllerSalvataggio.java
@@ -1,5 +1,6 @@
 package restaurantsim.controller;
 
+import restaurantsim.model.Classifica;
 import restaurantsim.model.Sala;
 import restaurantsim.view.MainPanel;
 
@@ -7,4 +8,6 @@ public class ControllerSalvataggio {
 	// Questo raccoglie i dati da panel e model e li dà al model Salvataggio.java
 	private MainPanel mainPanel;
 	private Sala salaModel;
+	private Classifica classificaModel;
+	
 }
--- src/main/java/restaurantsim/model/SalvataggioObject.java
+++ src/main/java/restaurantsim/model/SalvataggioObject.java
@@ -7,15 +7,17 @@ public class SalvataggioObject {
 	// Qui ci vanno i dati ricevuti dal controller, sennò niente MVC
 	private ArrayList<Piatto> piattiPronti;
 	private LinkedList<String> notifiche;
+	private ArrayList<Cuoco> cuochi;
 	private ArrayList<Tavolo> tavoli;
+	private ArrayList<String> classifica;
 
-	public SalvataggioObject() {
-	}
+	public SalvataggioObject() {}
 
-	public SalvataggioObject(ArrayList<Piatto> piattiPronti, LinkedList<String> notifiche, ArrayList<Tavolo> tavoli) {
+	public SalvataggioObject(ArrayList<Piatto> piattiPronti, LinkedList<String> notifiche, ArrayList<Cuoco> cuochi, ArrayList<Tavolo> tavoli, ArrayList<String> classifica) {
 		this.piattiPronti = piattiPronti;
 		this.notifiche = notifiche;
+		this.cuochi = cuochi;
 		this.tavoli = tavoli;
+		this.classifica = classifica;
 	}
-
 }
